80a1f354574b7dbc7fdb410d0fdb49192e2f3fd1
Pyunghyuk Yoo <yoophi@gmail.com>
2016-10-24 18:38:06 +0900

    내부 템플릿을 쓰지 않도록 함

--- flask_api_app/__init__.py
+++ flask_api_app/__init__.py
@@ -3,16 +3,12 @@ import os
 from flask import Flask
 from flask_api_app.extensions import init_extensions
 
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 
 
 class FlaskApiApp(Flask):
     def __init__(self, import_name, static_path=None, static_url_path=None, static_folder='static',
-                 template_folder=None, instance_path=None, instance_relative_config=False):
-        if not template_folder:
-            current_dir = os.path.dirname(__file__)
-            template_folder = os.path.join(current_dir, 'templates')
-
+                 template_folder='templates', instance_path=None, instance_relative_config=False):
         super(FlaskApiApp, self).__init__(import_name, static_path, static_url_path, static_folder, template_folder,
                                           instance_path, instance_relative_config)
 
